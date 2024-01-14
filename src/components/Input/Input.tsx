9df660ac2798b3c192dbadca05efb235a9ba3b6e
Yatmanov Evgeniy <nokiabagpipes@gmail.com>
2024-01-14 09:59:48 +0300

    feat: unFormattingValue

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,57 @@
 import React, { useId } from 'react';
 import classes from './Input.module.scss';
 
+interface Formatter {
+  patternChar: string;
+  format: string;
+}
+
+const formattingValue = (value: string, formatter?: Formatter): string => {
+  if (!formatter) {
+    return value;
+  }
+  let formattedValue = '';
+  const { format, patternChar } = formatter;
+  for (let i = 0; i < format.length; i += 1) {
+    const charFormat = format[i];
+    const charInputValue = value?.[i];
+    if (charInputValue !== undefined) {
+      if (charFormat === patternChar) {
+        formattedValue = formattedValue.concat(charInputValue);
+      } else if (charFormat !== charInputValue) {
+        formattedValue = formattedValue.concat(charFormat).concat(charInputValue);
+      } else {
+        formattedValue = formattedValue.concat(charFormat);
+      }
+    }
+  }
+  return formattedValue;
+};
+
+const unFormattingValue = (value: string, formatter?: Formatter): string => {
+  if (!formatter) {
+    return value;
+  }
+  const { format, patternChar } = formatter;
+  let unFormattedValue = '';
+  for (let i = 0; i < format.length; i += 1) {
+    const charFormat = format[i];
+    const charValue = value?.[i];
+    if (charValue !== undefined) {
+      if (charFormat === patternChar) {
+        unFormattedValue = unFormattedValue.concat(charValue);
+      }
+    }
+  }
+  return unFormattedValue;
+};
+
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   error: Record<string, boolean>;
   fieldName: string;
   label: string;
   hideValue?: boolean;
-  formatter?:
-    | {
-        patternChar: string;
-        format: string;
-      }
-    | undefined;
+  formatter?: Formatter | undefined;
   textAlign?: React.CSSProperties['textAlign'];
 }
 
@@ -57,27 +97,16 @@ const Input: React.FC<Props> = ({
       });
       return;
     }
-    if (formatter) {
-      let formattedValue = '';
-      const { format, patternChar } = formatter;
-      for (let i = 0; i < format.length; i += 1) {
-        const charFormat = format[i];
-        const charInputValue = inputValue?.[i];
-        if (charInputValue !== undefined) {
-          if (charFormat === patternChar) {
-            formattedValue = formattedValue.concat(charInputValue);
-          } else if (charFormat !== charInputValue) {
-            formattedValue = formattedValue.concat(charFormat).concat(charInputValue);
-          } else {
-            formattedValue = formattedValue.concat(charFormat);
-          }
-        }
-      }
-      changeFormatValue(formattedValue);
-    } else {
-      changeFormatValue(inputValue);
-    }
-    props.onChange(e);
+
+    changeFormatValue(formattingValue(inputValue, formatter));
+
+    props.onChange({
+      ...e,
+      target: {
+        ...e.target,
+        value: unFormattingValue(e.target.value, formatter),
+      },
+    });
   };
   return (
     <div className={classes.root} style={style}>
