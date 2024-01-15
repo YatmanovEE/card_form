1d4be47e06580b2561addb0ec3528f18359dee0a
Yatmanov Evgeniy <nokiabagpipes@gmail.com>
2024-01-15 22:07:47 +0300

    feat: validDate
    docs: function descripbes

--- src/validators/validation.test.ts
+++ src/validators/validation.test.ts
@@ -1,4 +1,4 @@
-import { intervalLength, required, maxLength, wordsCount } from './validators';
+import { intervalLength, required, maxLength, wordsCount, validDate } from './validators';
 
 describe('Проверка интервальной длинны', () => {
   const intervalTest = intervalLength(1, 10);
@@ -42,14 +42,41 @@ describe('Проверка обязательности заполнения', (
 });
 
 describe('Проверка на количество слов', () => {
-  const wordsCountText = wordsCount(2);
+  const wordsCountTest = wordsCount(2);
   test('Необходимое количество слов', () => {
-    expect(wordsCountText('Test Test')).toBe(true);
+    expect(wordsCountTest('Test Test')).toBe(true);
   });
   test('Количество слов меньше', () => {
-    expect(wordsCountText('test')).toBe(false);
+    expect(wordsCountTest('test')).toBe(false);
   });
   test('Количество слов больше', () => {
-    expect(wordsCountText('test test test')).toBe(false);
+    expect(wordsCountTest('test test test')).toBe(false);
+  });
+});
+
+describe('Проверка даты', () => {
+  it('Корректная дата', () => {
+    const validDateString = '06/23';
+    expect(validDate(validDateString)).toBe(true);
+  });
+
+  it('Некорректным месяц', () => {
+    const invalidMonthDateString = '13/23';
+    expect(validDate(invalidMonthDateString)).toBe(false);
+  });
+
+  it('Некорректный год', () => {
+    const invalidYearDateString = '06/99';
+    expect(validDate(invalidYearDateString)).toBe(false);
+  });
+
+  it('Некорректным формат', () => {
+    const invalidFormatDateString = '0623';
+    expect(validDate(invalidFormatDateString)).toBe(false);
+  });
+
+  it('Некорректный разделитель', () => {
+    const invalidSeparatorDateString = '06-23';
+    expect(validDate(invalidSeparatorDateString)).toBe(false);
   });
 });
--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -1,34 +1,48 @@
 import { IValidator } from '../components/Common';
 
+/**
+ * Валидатор для проверки, что значение не пусто.
+ */
 export const required: IValidator = (value) => {
-  if (!value) {
-    return false;
-  }
-  return true;
+  return !!value;
 };
 
+/**
+ * Валидатор для проверки максимальной длины строки.
+ * @param length Максимальная длина строки.
+ */
 export const maxLength =
   (length: number): IValidator =>
   (value) => {
-    if (value.length > length) {
-      return false;
-    }
-    return true;
+    return value.length <= length;
   };
 
+/**
+ * Валидатор для проверки количества слов в строке.
+ * @param length Ожидаемое количество слов.
+ */
 export const wordsCount =
   (length: number): IValidator =>
   (value) => {
-    if (value.split(' ').length === length) return true;
-    return false;
+    return value.split(' ').length === length;
   };
 
+/**
+ * Валидатор для проверки, что длина строки находится в заданном интервале.
+ * @param min Минимальная длина строки.
+ * @param max Максимальная длина строки.
+ */
 export const intervalLength =
   (min: number, max: number): IValidator =>
   (value) => {
-    if (value.length < min || value.length > max) {
-      return false;
-    }
-
-    return true;
+    return value.length >= min && value.length <= max;
   };
+
+/**
+ * Валидатор для проверки даты в формате MM/YY.
+ */
+export const validDate: IValidator = (value) => {
+  const [month, year] = value.split('/').map(Number);
+  // Проверка, что месяц находится в диапазоне от 1 до 12, а год в диапазоне от 21 до 26
+  return month >= 1 && month <= 12 && year >= 21 && year <= 26;
+};
